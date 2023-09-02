bdb6aad484057e4959a398957e984ef549bf8055
madubuana <143859212+madubuana@users.noreply.github.com>
2023-09-02 10:42:54 +0700

    first commit

---
+++ operatoraritmatika.py
@@ -0,0 +1,13 @@
+# penjumlahan
+print(34+2)
+# pengurangan 
+print(30-4)
+# perkalian
+print(2*6)
+# pembagian 
+print(9/3)
+
+# kalau di simpan di dlm variabel
+a = 39
+b = 4
+print (a // b)
---
+++ tipedata.py
@@ -0,0 +1,26 @@
+# int dan float
+a = 52.2
+print (a) 
+print (type(a))
+a2 = 52
+#  str
+print (type(a2))
+nama_apel = "malang"
+print (nama_apel)
+print (type(nama_apel))
+#  bool
+apel_benar = True
+apel_salah = False
+print(type(apel_benar))
+print(apel_salah)
+# list 
+list_warna_apel = ['hijau', 'merah', 'kuning']
+list_apel1 = ["manis", 2, 3.0]
+print(list_warna_apel)
+print(list_apel1)
+# tuple
+apel_tuple = (2,  5.75,  "tiga")
+print(apel_tuple)
+# dictionary 
+apel_dictionary = {'buah': 'apel', 'warna': 'hijau', 'jumlah': '5'}
+print(apel_dictionary['buah'])
